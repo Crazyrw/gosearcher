bd46a8a7abd5f29aeb100256a8cd99032f9a430b
genleel <imelolee@gmail.com>
2022-06-11 10:01:26 +0800

    user service update

--- web/controller/base.go
+++ web/controller/base.go
@@ -1,35 +1,39 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"goSearcher/searcher/core"
-	"goSearcher/searcher/model"
-	"goSearcher/searcher/utils"
-	"goSearcher/searcher/words"
 	"net/http"
 )
 
 func Index(c *gin.Context) {
 	//userInfo := getCurrentUser(c)
 
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{"login": false})
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 }
 
 func Query(c *gin.Context) {
-	var documents []model.Docs
-	content := c.Query("content")
-	//cut content to many terms by cut model
-	tokenizer := words.NewTokenizer()
-	words := tokenizer.CutContent(content)
+	//var documents []model.Docs
+	//content := c.Query("content")
+	////cut content to many terms by cut model
+	//tokenizer := words.NewTokenizer()
+	//words := tokenizer.CutContent(content)
 	//search in index
-	for _, item := range words {
-		if value, ok := core.MemoryBTree.Find(item); ok {
-			docIds := utils.SplitDocIdsFromValue(fmt.Sprintf("%v", value))
-			docs := core.GetDocuments(docIds)
-			documents = append(documents, docs...)
-		}
-	}
+	//for _, item := range words {
+	//	if value, ok := core.MemoryBTree.Find(item); ok {
+	//		docIds := utils.SplitDocIdsFromValue(fmt.Sprintf("%v", value))
+	//		docs := core.GetDocuments(docIds)
+	//		documents = append(documents, docs...)
+	//	}
+	//}
 	//continue to score
 	//....
+
+	// 模拟数据
+	resp := gin.H{
+		"url":   "https://go.dev/",
+		"title": "The Go Programming Language",
+		"img":   "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1142154201444823041%2FO6AczwfV_400x400.png&imgrefurl=https%3A%2F%2Ftwitter.com%2Fgolang&tbnid=AfPvlfaD_83mKM&vet=12ahUKEwjhu8OG-qH4AhWJS_UHHf6pAG8QMygAegUIARCGAQ..i&docid=KnPnz5GJJTt4DM&w=400&h=400&q=go&ved=2ahUKEwjhu8OG-qH4AhWJS_UHHf6pAG8QMygAegUIARCGAQ",
+	}
+
+	c.HTML(http.StatusOK, "index.tmpl", resp)
 }
--- web/model/user.go
+++ web/model/user.go
@@ -16,13 +16,13 @@ type User struct {
 }
 
 type UserSave struct {
-	User    User `gorm:"ForeignKey:UserId;AssociationForeignKey:ID"`
-	UserId  int
-	SaveUrl string `gorm:"not null"`
+	User      User `gorm:"ForeignKey:UserId;AssociationForeignKey:ID"`
+	UserId    int
+	SaveDocId int `gorm:"not null"`
 }
 
 func UserDBInit() {
-	dsn := "ligen:LiGen1129!@tcp(127.0.0.1:3306)/goSearcher?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:122513gzhGZH!!@tcp(decs.pcl.ac.cn:1762)/search_engine?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
