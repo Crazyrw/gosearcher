1e1c913c825f2ac97475d13704bed083ef5dac80
Crazyrw <2096498443@qq.com>
2022-06-11 19:01:28 +0800

    score testing

--- web/controller/base.go
+++ web/controller/base.go
@@ -3,11 +3,11 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"goSearcher/searcher/core"
+	"goSearcher/searcher/rank"
 	"goSearcher/searcher/relate_search"
 	"goSearcher/searcher/utils"
 	"goSearcher/searcher/words"
 	"goSearcher/web/result"
-	"math"
 	"net/http"
 )
 
@@ -43,95 +43,13 @@ func Query(c *gin.Context) {
 		result.Error("no results")
 	}
 	//to score: get new docIds
-
+	rankDocIds := rank.Rank(content, docIds)
 	//relate search
 	relatedSearchQueries := relate_search.GetRelatedSearchQueries(content, docIds)
 
 	result.ResponseSuccessWithData(c, result.QueryResult{
 		RelatedSearch: relatedSearchQueries,
-		Documents:     docIds,
+		Documents:     rankDocIds,
 	})
 
-}
-
-func rank(query string, docIds []int) (docIdsRank []int) {
-
-	documents := utils.GetDocuments(docIds)
-	var docIdsForRank []int //存放要排序的docid
-	var docs []string       //存放要排序的caption
-	for _, item := range documents {
-		docs = append(docs, item.Caption)
-		docIdsForRank = append(docIdsForRank, item.ID)
-	}
-
-	tokenizer := words.NewTokenizer()
-	queryWords := tokenizer.CutContent(query)         //query的分词结果
-	IDFs := make(map[string]float64, len(queryWords)) //保存query中每个word的idf值
-	for _, word := range queryWords {
-		var num float64 = 0
-		for _, doc := range docs {
-			docWords := tokenizer.CutContent(doc) //doc的分词结果
-			flag := Find(docWords, word)
-			if flag == true {
-				num++
-			}
-		}
-		//df := num / float64(len(docs))
-		idf := math.Log2(float64(len(docs)) / num)
-		IDFs[word] = idf
-	}
-
-	docsScore := make([]float64, len(docs), len(docs))
-	for k, doc := range docs {
-		score := getScore(query, doc, IDFs)
-		docsScore[k] = score
-	}
-
-	//保存所有的docId和对应的score
-	rankMap := make(map[int]float64, len(docs))
-	for index, score := range docsScore {
-		rankMap[docIdsForRank[index]] = score
-	}
-
-	return docIdsRank
-}
-
-//获取query对于某个doc的tf-idf值
-func getScore(query string, doc string, IDFs map[string]float64) (score float64) {
-	score = 0
-	//var DF float64 = 0
-	tokenizer := words.NewTokenizer()
-	queryWords := tokenizer.CutContent(query) //query的分词结果
-	docWords := tokenizer.CutDoc(doc)         //doc的分词结果
-	for _, word := range queryWords {
-		count := Count(docWords, word)
-		var tf float64 = count / float64(len(docWords))
-		idf := IDFs[word]
-		score += tf * idf
-	}
-
-	return score
-}
-
-//判断string切片中含某个string的个数
-func Count(slice []string, val string) (count float64) {
-	count = 0
-	for _, item := range slice {
-		if item == val {
-			count++
-		}
-	}
-	return count
-}
-
-//判断string切片中是否含某个string
-func Find(slice []string, val string) (flag bool) {
-	flag = false
-	for _, item := range slice {
-		if item == val {
-			flag = true
-			break
-		}
-	}
-	return flag
 }
