753753236616bf7432d39cf9440aea0ce8d5cb5d
genleel <imelolee@gmail.com>
2022-06-11 10:46:26 +0800

    user service update

--- web/controller/base.go
+++ web/controller/base.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
-<<<<<<< HEAD
-=======
+	"fmt"
 	"goSearcher/searcher/core"
 	"goSearcher/searcher/db"
 	"goSearcher/searcher/model"
@@ -12,9 +10,10 @@ import (
 	"goSearcher/web/result"
 	"log"
 	"math"
->>>>>>> 39830c156187479b44a4133abe0e0253d3952e65
 	"net/http"
 	"sort"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
@@ -23,32 +22,6 @@ func Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
 }
 func Query(c *gin.Context) {
-<<<<<<< HEAD
-	//var documents []model.Docs
-	//content := c.Query("content")
-	////cut content to many terms by cut model
-	//tokenizer := words.NewTokenizer()
-	//words := tokenizer.CutContent(content)
-	//search in index
-	//for _, item := range words {
-	//	if value, ok := core.MemoryBTree.Find(item); ok {
-	//		docIds := utils.SplitDocIdsFromValue(fmt.Sprintf("%v", value))
-	//		docs := core.GetDocuments(docIds)
-	//		documents = append(documents, docs...)
-	//	}
-	//}
-	//continue to score
-	//....
-
-	// 模拟数据
-	resp := gin.H{
-		"url":   "https://go.dev/",
-		"title": "The Go Programming Language",
-		"img":   "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F1142154201444823041%2FO6AczwfV_400x400.png&imgrefurl=https%3A%2F%2Ftwitter.com%2Fgolang&tbnid=AfPvlfaD_83mKM&vet=12ahUKEwjhu8OG-qH4AhWJS_UHHf6pAG8QMygAegUIARCGAQ..i&docid=KnPnz5GJJTt4DM&w=400&h=400&q=go&ved=2ahUKEwjhu8OG-qH4AhWJS_UHHf6pAG8QMygAegUIARCGAQ",
-	}
-
-	c.HTML(http.StatusOK, "index.tmpl", resp)
-=======
 
 	docIdsMap := make(map[int]int)
 	var docIds []int //union set
@@ -195,5 +168,4 @@ func getQueriesByIds(queryIds []int) (queries []string) {
 		queries = append(queries, item.Query)
 	}
 	return
->>>>>>> 39830c156187479b44a4133abe0e0253d3952e65
 }
--- web/model/user.go
+++ web/model/user.go
@@ -18,20 +18,6 @@ type UserSave struct {
 	SaveDocId int `gorm:"not null"`
 }
 
-<<<<<<< HEAD
-func UserDBInit() {
-	dsn := "root:122513gzhGZH!!@tcp(decs.pcl.ac.cn:1762)/search_engine?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&UserSave{})
-
-	fmt.Println("Finish migrate.")
-
-}
-=======
 //func UserDBInit() {
 //	dsn := "ligen:LiGen1129!@tcp(127.0.0.1:3306)/goSearcher?charset=utf8mb4&parseTime=True&loc=Local"
 //	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -44,4 +30,3 @@ func UserDBInit() {
 //	fmt.Println("Finish migrate.")
 //
 //}
->>>>>>> 39830c156187479b44a4133abe0e0253d3952e65
