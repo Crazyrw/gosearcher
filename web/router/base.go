93c4e106c83fc9524cee08f0d721d608d0bbe273
genleel <imelolee@gmail.com>
2022-06-12 16:46:59 +0800

    Results

--- web/router/base.go
+++ web/router/base.go
@@ -8,7 +8,9 @@ import (
 func InitBaseRouter(router *gin.RouterGroup) {
 	baseRouter := router.Group("base")
 	{
-		baseRouter.GET("/index", controller.Index) //index page
-		baseRouter.GET("/query", controller.Query) //user search keys
+		baseRouter.GET("/index", controller.Index)      //index page
+		baseRouter.GET("/query", controller.Query)      //user search keys
+		baseRouter.GET("/last", controller.GetLastPage) //user search keys
+		baseRouter.GET("/next", controller.GetNextPage) //user search keys
 	}
 }
